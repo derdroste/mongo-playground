d8249ced8c2c09b7feaceb19507ec658a95feb05
derdroste <dr.oste129@gmail.com>
2020-11-17 23:14:05 +0100

    mongoose comparison operators

--- app.js
+++ app.js
@@ -27,8 +27,19 @@ async function createCourse() {
 }
 
 async function getCourses() {
+    // eq => equal
+    // ne => not equal
+    // gt => greater than
+    // gte => greater/equal
+    // lt => less than
+    // lte => less than/equal
+    // in
+    //nin => not in
+
     const courses = await Course
-        .find({ author: 'Lukas', isPublished: true})
+      // .find({ author: 'Lukas', isPublished: true})
+      // .find({ price: { $gte: 10, $lte: 20 } })
+        .find({ price: { $in: [10, 15, 20 ]}})
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1})
