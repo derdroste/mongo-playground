9ee4b7051913bec009b25a69018c87aa38d8fa4b
derdroste <dr.oste129@gmail.com>
2020-11-18 21:21:32 +0100

    schematype options

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const courseSchema = new mongoose.Schema({
     category: {
         type: String,
         required: true,
-        enum: ['web', 'mobile', 'network']
+        enum: ['web', 'mobile', 'network'],
+        lowercase: true,
+        trim: true
     },
     author: String,
     tags: {
@@ -36,7 +38,9 @@ const courseSchema = new mongoose.Schema({
         type: Number,
         required: function() {return this.isPublished},
         min: 10,
-        max: 200
+        max: 200,
+        get: v => Math.round(v),
+        set: v => Math.round(v)
     }
 });
 
@@ -45,7 +49,7 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
     const course = new Course({
         name: 'Node.js Course',
-        category: '-',
+        category: 'Web',
         author: 'Lukas',
         tags: ['node', 'backend'],
         isPublished: true
