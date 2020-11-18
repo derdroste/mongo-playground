732a96a5ce56d7a46947dc552982ba76200cd835
derdroste <dr.oste129@gmail.com>
2020-11-18 12:27:55 +0100

    sort by price and title

--- get-all-pub-backend.js
+++ get-all-pub-backend.js
@@ -15,11 +15,9 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function getCourses() {
     await Course
-        .find({tags: 'backend'}, {tags: 'frontend'})
-        .sort({price: -1})
-        .select({ name: 1, author: 1, price: 1 })
+        .find({isPublished: true})
+        .or([{ price: { $gte: 15}}, { name: /.*by.*/i }])
         .then(result => console.log(result));
 }
-
 getCourses();
 
